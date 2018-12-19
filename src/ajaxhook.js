bf148a723e1048e6cc5254bed97059a0186e36ed
zjsxdyw <zjsxdyw@gmail.com>
2018-12-19 11:51:27 +0800

    More similar to the original

--- src/ajaxhook.js
+++ src/ajaxhook.js
@@ -3,52 +3,57 @@
  * email: 824783146@qq.com
  * source code: https://github.com/wendux/Ajax-hook
  **/
-module.exports=function (ob) {
-    ob.hookAjax = function (proxy) {
+module.exports = function(ob) {
+    ob.hookAjax = function(proxy) {
         window._ahrealxhr = window._ahrealxhr || XMLHttpRequest
-        XMLHttpRequest = function () {
-            this.xhr = new window._ahrealxhr;
-            for (var attr in this.xhr) {
-                var type = "";
-                try {
-                    type = typeof this.xhr[attr]
-                } catch (e) {}
-                if (type === "function") {
-                    this[attr] = hookfun(attr);
-                } else {
-                    Object.defineProperty(this, attr, {
-                        get: getFactory(attr),
-                        set: setFactory(attr),
-                        enumerable: true
-                    })
-                }
+        XMLHttpRequest = function() {
+            var xhr = new window._ahrealxhr;
+            Object.defineProperty(this, 'xhr', {
+                value: xhr
+            })
+        }
+
+        var prototype = window._ahrealxhr.prototype;
+        for (var attr in prototype) {
+            var type = "";
+            try {
+                type = typeof prototype[attr]
+            } catch (e) {}
+            if (type === "function") {
+                XMLHttpRequest.prototype[attr] = hookfun(attr);
+            } else {
+                Object.defineProperty(XMLHttpRequest.prototype, attr, {
+                    get: getFactory(attr),
+                    set: setFactory(attr),
+                    enumerable: true
+                })
             }
         }
 
         function getFactory(attr) {
-            return function () {
-                var v= this.hasOwnProperty(attr + "_")?this[attr + "_"]:this.xhr[attr];
-                var attrGetterHook=(proxy[attr]||{})["getter"]
-                return attrGetterHook&&attrGetterHook(v,this)||v
+            return function() {
+                var v = this.hasOwnProperty(attr + "_") ? this[attr + "_"] : this.xhr[attr];
+                var attrGetterHook = (proxy[attr] || {})["getter"]
+                return attrGetterHook && attrGetterHook(v, this) || v
             }
         }
 
         function setFactory(attr) {
-            return function (v) {
+            return function(v) {
                 var xhr = this.xhr;
                 var that = this;
-                var hook=proxy[attr];
-                if (typeof hook==="function") {
-                    xhr[attr] = function () {
+                var hook = proxy[attr];
+                if (typeof hook === "function") {
+                    xhr[attr] = function() {
                         proxy[attr](that) || v.apply(xhr, arguments);
                     }
                 } else {
                     //If the attribute isn't writeable, generate proxy attribute
-                    var attrSetterHook=(hook||{})["setter"];
-                    v=attrSetterHook&&attrSetterHook(v,that)||v
+                    var attrSetterHook = (hook || {})["setter"];
+                    v = attrSetterHook && attrSetterHook(v, that) || v
                     try {
                         xhr[attr] = v;
-                    }catch(e) {
+                    } catch (e) {
                         this[attr + "_"] = v;
                     }
                 }
@@ -56,7 +61,7 @@ module.exports=function (ob) {
         }
 
         function hookfun(fun) {
-            return function () {
+            return function() {
                 var args = [].slice.call(arguments)
                 if (proxy[fun] && proxy[fun].call(this, args, this.xhr)) {
                     return;
@@ -66,10 +71,10 @@ module.exports=function (ob) {
         }
         return window._ahrealxhr;
     }
-    ob.unHookAjax = function () {
-        if (window._ahrealxhr)  XMLHttpRequest = window._ahrealxhr;
+    ob.unHookAjax = function() {
+        if (window._ahrealxhr) XMLHttpRequest = window._ahrealxhr;
         window._ahrealxhr = undefined;
     }
     //for typescript
-    ob["default"]=ob;
+    ob["default"] = ob;
 }
