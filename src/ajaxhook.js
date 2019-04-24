3925c8b0abd33210f6471874db6b8d15a6fd9ca0
杜文 <duwen.x@bytedance.com>
2019-04-24 16:40:28 +0800

    fix #26

--- src/ajaxhook.js
+++ src/ajaxhook.js
@@ -1,54 +1,68 @@
 /*
- * author: wendu
+ * author: wendux
  * email: 824783146@qq.com
  * source code: https://github.com/wendux/Ajax-hook
  **/
-module.exports = function(ob) {
-    ob.hookAjax = function(proxy) {
-        window._ahrealxhr = window._ahrealxhr || XMLHttpRequest
-        XMLHttpRequest = function() {
-            var xhr = new window._ahrealxhr;
-            Object.defineProperty(this, 'xhr', {
-                value: xhr
-            })
-        }
+module.exports = function (ob) {
 
-        var prototype = window._ahrealxhr.prototype;
-        for (var attr in prototype) {
-            var type = "";
-            try {
-                type = typeof prototype[attr]
-            } catch (e) {}
-            if (type === "function") {
-                XMLHttpRequest.prototype[attr] = hookfun(attr);
-            } else {
-                Object.defineProperty(XMLHttpRequest.prototype, attr, {
-                    get: getFactory(attr),
-                    set: setFactory(attr),
-                    enumerable: true
-                })
+    //Save original XMLHttpRequest as RealXMLHttpRequest
+    var realXhr = "RealXMLHttpRequest"
+
+    //Call this function will override the `XMLHttpRequest` object
+    ob.hookAjax = function (proxy) {
+
+        // Avoid double hook
+        window[realXhr] = window[realXhr] || XMLHttpRequest
+
+        XMLHttpRequest = function () {
+            var xhr = new window[realXhr];
+            // We shouldn't hook XMLHttpRequest.prototype because we can't
+            // guarantee that all attributes are on the prototype。
+            // Instead, hooking XMLHttpRequest instance can avoid this problem.
+            for (var attr in xhr) {
+                var type = "";
+                try {
+                    type = typeof xhr[attr] // May cause exception on some browser
+                } catch (e) {
+                }
+                if (type === "function") {
+                    // hook methods of xhr, such as `open`、`send` ...
+                    this[attr] = hookFunction(attr);
+                } else {
+                    Object.defineProperty(this, attr, {
+                        get: getterFactory(attr),
+                        set: setterFactory(attr),
+                        enumerable: true
+                    })
+                }
             }
+            this.xhr = xhr;
+
         }
 
-        function getFactory(attr) {
-            return function() {
+        // Generate getter for attributes of xhr
+        function getterFactory(attr) {
+            return function () {
                 var v = this.hasOwnProperty(attr + "_") ? this[attr + "_"] : this.xhr[attr];
                 var attrGetterHook = (proxy[attr] || {})["getter"]
                 return attrGetterHook && attrGetterHook(v, this) || v
             }
         }
 
-        function setFactory(attr) {
-            return function(v) {
+        // Generate setter for attributes of xhr; by this we have an opportunity
+        // to hook event callbacks （eg: `onload`） of xhr;
+        function setterFactory(attr) {
+            return function (v) {
                 var xhr = this.xhr;
                 var that = this;
                 var hook = proxy[attr];
                 if (typeof hook === "function") {
-                    xhr[attr] = function() {
+                    // hook  event callbacks such as `onload`、`onreadystatechange`...
+                    xhr[attr] = function () {
                         proxy[attr](that) || v.apply(xhr, arguments);
                     }
                 } else {
-                    //If the attribute isn't writeable, generate proxy attribute
+                    //If the attribute isn't writable, generate proxy attribute
                     var attrSetterHook = (hook || {})["setter"];
                     v = attrSetterHook && attrSetterHook(v, that) || v
                     try {
@@ -60,8 +74,9 @@ module.exports = function(ob) {
             }
         }
 
-        function hookfun(fun) {
-            return function() {
+        // Hook methods of xhr.
+        function hookFunction(fun) {
+            return function () {
                 var args = [].slice.call(arguments)
                 if (proxy[fun] && proxy[fun].call(this, args, this.xhr)) {
                     return;
@@ -69,12 +84,17 @@ module.exports = function(ob) {
                 return this.xhr[fun].apply(this.xhr, args);
             }
         }
-        return window._ahrealxhr;
+
+        // Return the real XMLHttpRequest
+        return window[realXhr];
     }
-    ob.unHookAjax = function() {
-        if (window._ahrealxhr) XMLHttpRequest = window._ahrealxhr;
-        window._ahrealxhr = undefined;
+
+    // Cancel hook
+    ob.unHookAjax = function () {
+        if (window[realXhr]) XMLHttpRequest = window[realXhr];
+        window[realXhr] = undefined;
     }
+
     //for typescript
     ob["default"] = ob;
 }
