beff7fc1417754f9a4542b414ee5885ee13a643c
chenyingyan.xy <chenyingyan.xy@bytedance.com>
2023-06-20 16:17:05 +0800

    fix: export error

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
  * email: 824783146@qq.com
  * source code: https://github.com/wendux/Ajax-hook
  **/
-export {hook, unHook}from "./src/xhr-hook"
-export {proxy, unProxy} from "./src/xhr-proxy"
+export { hook }from "./src/xhr-hook"
+export { proxy } from "./src/xhr-proxy"
 
